c96d089a596cd7c4baf33cd4984c3ff52f6d6b86
freemjstudio <mjwoo0@naver.com>
2024-05-06 12:54:33 +0900

    회전 초밥

--- 현대모비스/네 수.py
+++ 현대모비스/네 수.py
@@ -1,1 +1,7 @@
 # https://www.acmicpc.net/problem/10824
+a, b, c, d = input().split()
+front = a+b
+back = c+d
+answer = int(front) + int(back)
+print(answer)
+
--- 현대모비스/회전초밥.py
+++ 현대모비스/회전초밥.py
@@ -1,1 +1,14 @@
 # https://www.acmicpc.net/problem/2531
+N, d, k, c = map(int, input().split())
+answer = 0
+arr = []
+
+for _ in range(N):
+    arr.append(int(input()))
+sushi = arr + arr
+
+for i in range(N):
+    dish = sushi[i:i+k]
+    dish.append(c)
+    answer = max(answer, len(set(dish)))
+print(answer)
