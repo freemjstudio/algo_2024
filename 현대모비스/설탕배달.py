47ffcb52dd41ad927a1d32779942645e6ee130b7
freemjstudio <mjwoo0@naver.com>
2024-05-09 07:31:18 +0900

    자물쇠와 열쇠

--- 현대모비스/설탕배달.py
+++ 현대모비스/설탕배달.py
@@ -1,2 +1,37 @@
-# https://www.acmicpc.net/problem/2839
+< script
+src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" > < / script >
 
+< script
+type = "text/javascript" >
+$(document).ready(function()
+{
+$('h1,h2,h3,h4,p,span').removeAttr('data-ke-size').css('color', '');;
+});
+< / script >
+< script >
+document.addEventListener("DOMContentLoaded", function()
+{
+
+// Remove & nbsp; and & gt;
+from HTML content
+
+var
+allElements = document.querySelectorAll("*");
+
+for (var i = 0; i < allElements.length; i++)
+{
+    var
+element = allElements[i];
+var
+htmlContent = element.innerHTML;
+
+// Replace & nbsp; with space
+htmlContent = htmlContent.replace( / & nbsp; / g, ' ');
+
+// Replace & gt; with >
+htmlContent = htmlContent.replace( / & gt; / g, '>');
+
+element.innerHTML = htmlContent;
+}
+});
+< / script >
