092d6eb307e9532bbd4c84c7cca1ab25928afc2d
freemjstudio <mjwoo0@naver.com>
2024-06-15 10:29:28 +0900

    day14

--- challenge/day13/sudoku_solver.py
+++ challenge/day13/sudoku_solver.py
@@ -5,13 +5,25 @@ class Solution:
         # 백트래킹 
         answer_set = set([i for i in range(1, 10)])
         
-        def backtracking(x, y): # 현재 좌표 
-            temp_set = set()
+        def check_valid():
             for i in range(9):
-                
-                if arr[i][x]
+                for j in range(9):
+                    if board[i][j] == '.':
+                        return False 
+            return True 
         
+        def backtracking(x, y): # 현재 좌표 
+            
+            temp_set = set()
+            if check_valid():
+                return # 재귀 함수 탈출 
+            for i in range(9):
+                for j in range(9):
+                    if arr[i][x] == '':
+                    
+        
+        # 함수 실행 
         for i in range(9):
             for j in range(9):
                 if board[i][j] == ".":
-                    backtracking(0, 0)    
+                    backtracking(i, j)    
