11740b70f7ad6b6c20ecdca481e0c5f6dd55e753
freemjstudio <mjwoo0@naver.com>
2024-06-18 18:28:51 +0900

    container with most water

--- challenge/day15/container_with_most_water.py
+++ challenge/day15/container_with_most_water.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/container-with-most-water/
 
+from typing import List
+
 '''
 You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
@@ -15,24 +17,23 @@ Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,
 
 '''
 
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         
         n = len(height)
         answer = 0 
-        left, right = 1, n
+        left, right = 0, n-1
         while left < right: 
-            w = (right - left) + 1
-            h = min(height[left], height[right])
+            w = (right - left)
+            h = min(height[left], height[right]) # 둘중에 작은 높이가 기준이 됨! 
             answer = max(answer, w*h)
 
             # 어느 조건에서 left += 1 혹은 right -= 1
-            left += 1
-            right -= 1
-
+            if height[left] < height[right]:
+                left += 1
+            else: 
+                right -= 1
         return answer 
-    
 
 # Test Code 
 s = Solution()
