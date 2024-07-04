c2da7f84460ad5c1cdb0e2f62aa366d662b87eea
freemjstudio <mjwoo0@naver.com>
2024-07-04 17:36:32 +0900

    leetcode spiral matrix

---
+++ challenge/day19/spiral_matrix.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/spiral-matrix/
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+        
+        # 우, 하, 좌, 상 
+        dx = []
+        dy = []
+        
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        while 
+        
+        return result 
+        
