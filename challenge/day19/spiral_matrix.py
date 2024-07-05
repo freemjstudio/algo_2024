725de8be56f42e5826902f7d2db9c81f608c59dc
freemjstudio <mjwoo0@naver.com>
2024-07-06 03:21:49 +0900

    spiral_matrix

--- challenge/day19/spiral_matrix.py
+++ challenge/day19/spiral_matrix.py
@@ -1,29 +1,37 @@
 # https://leetcode.com/problems/spiral-matrix/
+from typing import List
+
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         answer = []
         
         # 우, 하, 좌, 상 
-        dx = [1, 0, -1, 0]
-        dy = [0, 1, 0, -1]
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
         
         n = len(matrix)
         m = len(matrix[0])
         visited = [[False] * m for _ in range(n)]
         total = n*m # matrix 칸의 개수 
         count = 0 
-        dir = 0 # 방향키 
+        d = 0 # 방향키 
 
         x, y = 0, 0 
+        
         for i in range(n*m):
-            
-            if visited[x][y] == True or not (0 <= x < n) or not (0 <= y > m):
-                # 방향 회전하기 
-                dir = (dir+1) % 4
+            answer.append(matrix[x][y])
             visited[x][y] = True 
-            x, y = x + dx[dir], y + dy[dir]
-            
+            x, y = x + dx[d], y + dy[d]
+            if x < 0 or x >= n or y < 0 or y >= m or visited[x][y] == True:
+                x, y = x - dx[d], y - dy[d]
+                d = (d+1) % 4
+                x, y = x + dx[d], y + dy[d]
         
         return answer 
-        
+
+t = [[1,2,3]
+     ,[4,5,6]
+     ,[7,8,9]]
+s = Solution()
+print(s.spiralOrder(t))
