14836425194a451407c282c20b52440fc225439f
freemjstudio <mjwoo0@naver.com>
2024-07-16 18:50:32 +0900

    maximum tree sum (answer)

--- challenge/day25/maxinum_tree_sum.py
+++ challenge/day25/maxinum_tree_sum.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
 
+from typing import Optional
+
 """
 
 A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. 
@@ -24,22 +26,37 @@ Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
 
 """
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        answer = -int(1e9)
 
-        def dfs(root, max_total):
+        def dfs(root):
+            nonlocal answer 
+
             if root is None: # node 
                 return 0
             
-            left_max = max(dfs(root.left, max_total), 0)
-            right_max = max(dfs(root.right, max_total), 0)
+            left_max = max(dfs(root.left), 0)
+            right_max = max(dfs(root.right), 0)
             
-            max_total = max(max_total, root.val + left_max + right_max)
+            answer = max(answer, root.val + left_max + right_max)
             
             return root.val + max(left_max, right_max)
         
-        return dfs(root, root.val)
+        dfs(root)
+        return answer
 
+
+#### Test Case 
+# 답이 6이 나와야 함 
+
+testcase = TreeNode(val= 1, left= TreeNode(val= 2, left= None, right= None), right= TreeNode(val= 3, left= None, right= None))
 s = Solution()
-r = [-10,9,20,null,null,15,7]
-s.maxPathSum(root=)
+answer = s.maxPathSum(root=testcase)
+print(answer)
