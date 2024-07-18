b241b1ac262545badb74d99c8a7b7a2c01abe076
freemjstudio <mjwoo0@naver.com>
2024-07-18 18:28:19 +0900

    가장 많이 받은 선물

--- challenge/day22/가장많이받은선물.py
+++ challenge/day22/가장많이받은선물.py
@@ -31,18 +31,43 @@ friends	gifts	result
 
 """
 
+from collections import defaultdict
+
 def solution(friends, gifts):
 
-    answer = 0
+
+    give_log = {f : {f:0 for f in friends} for f in friends} # {'muzi': {}, 'ryan': {}, 'frodo': {}, 'neo': {}}
+    receive_log = {f : {f:0 for f in friends} for f in friends} 
 
     for gift in gifts: 
         gift = gift.split()
         giver, receiver = gift[0], gift[1]
+        give_log[giver][receiver] += 1 # 준 기록 
+        receive_log[receiver][giver] += 1 # 받은 기록 
 
-        
+    score = {f : sum(receive_log[f].values()) for f in friends}
+    for me in friends: 
+        for friend in friends: 
+            if me == friend:
+                continue
+            # 준 선물의 수 > 받은 선물의 수 
+            if give_log[me][friend] > receive_log[me][friend]:
+                score[me] += 1
+            elif give_log[me][friend] < receive_log[me][friend]:
+                score[friend] += 1
+            else: # 선물 지수 구하기 
+                my_gift_score = sum(give_log[me].values()) - sum(receive_log[me].values())
+                friend_gift_score = sum(give_log[friend].values()) - sum(receive_log[me].values())
+                if my_gift_score > friend_gift_score:
+                    score[me] += 1
+                elif my_gift_score < friend_gift_score:
+                    score[friend] += 1
+                else: 
+                    continue 
 
 
-    return answer
+    # 가장 많이 선물을 받은 개수 반환 
+    return max(score.values())
 
 ### TEST CASE ### 
 
