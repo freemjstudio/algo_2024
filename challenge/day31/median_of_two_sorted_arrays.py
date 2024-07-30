63fb23ef05b48d74c50a18bec86ae56c19fe94c2
freemjstudio <mjwoo0@naver.com>
2024-07-31 07:21:35 +0900

    median of two sorted arrays

--- challenge/day31/median_of_two_sorted_arrays.py
+++ challenge/day31/median_of_two_sorted_arrays.py
@@ -37,14 +37,29 @@ class Solution:
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        arr = nums1 + nums2
-        n = len(arr)
-        arr.sort() 
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-        mid = n//2
-        if n%2 == 0: # 짝수인 경우 
-            
-            return arr[mid]
-        else: # 홀수인 경우 
-            
-            return arr[mid]
+        n = len(nums1)
+        m = len(nums2)
+        left, right = 0, n
+        while left <= right:
+            i = (right - left + 1) // 2 + left
+            j = (n + m + 1) // 2 - i
+            nums1_1 = nums1[i - 1] if i > 0 else -sys.maxsize - 1
+            nums1_2 = nums1[i] if i < n else sys.maxsize
+            nums2_1 = nums2[j - 1] if j > 0 else -sys.maxsize - 1
+            nums2_2 = nums2[j] if j < m else sys.maxsize
+            if nums1_1 > nums2_2:
+                right = i - 1
+            elif nums2_1 > nums1_2:
+                left = i + 1
+            else:
+                leftMax = max(nums1_1, nums2_1)
+                rightMin = min(nums1_2, nums2_2)
+                if (n + m) % 2 != 0:
+                    return leftMax
+                else:
+                    return (leftMax + rightMin) / 2.0
+
+        return 0.0
