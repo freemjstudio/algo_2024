0154193b3d1cdac416af10af1c3ea4a2f5703081
freemjstudio <mjwoo0@naver.com>
2024-07-30 11:28:58 +0900

    O(m+n) solution

---
+++ challenge/day31/median_of_two_sorted_arrays.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+# https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
+# O(m+n)
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n = len(nums1)
+        m = len(nums2)
+        t = (n+m) # length of the merged array 
+
+        left, right = 0, 0 # cursor for nums1, cursor for nums2
+        merged_arr = []
+
+        while left < n and right < m:
+            if nums1[left] <= nums2[right]:
+                merged_arr.append(nums1[left])
+                left += 1
+            else: 
+                merged_arr.append(nums2[right])
+                right += 1
+        
+        # 남은 숫자 
+        if left < n: 
+            merged_arr.extend(nums1[left:])
+        if right < m:
+            merged_arr.extend(nums2[right:])
+            
+        if len(merged_arr) % 2 == 0: # 짝수인 경우 
+            mid = len(merged_arr)//2
+            return (merged_arr[mid] + merged_arr[mid-1])/2 
+        else: # 홀수인 경우 
+            mid = len(merged_arr)//2 
+            return merged_arr[mid]
