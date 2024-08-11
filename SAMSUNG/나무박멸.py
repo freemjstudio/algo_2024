326bf29aac55295d80fefda803d845a261bc31b2
freemjstudio <mjwoo0@naver.com>
2024-08-11 15:58:48 +0900

    나무 박멸.py

--- SAMSUNG/나무박멸.py
+++ SAMSUNG/나무박멸.py
@@ -1,10 +1,8 @@
-# https://www.codetree.ai/training-field/frequent-problems/problems/tree-kill-all/description?page=1&pageSize=20&name=%EB%82%98%EB%AC%B4
-
 n, m, k, c = map(int, input().split())
 
 answer = 0 # m 년 동안 총 박멸할 나무 그루 수
 arr = []
-killer_arr = [] # 제초제 위치와 남은 년수 기록하기, 벽의 위치 정보는 필요함
+killer_arr = [[0] * n for _ in range(n)] # 제초제 위치와 남은 년수 기록하기, 벽의 위치 정보는 필요함
 
 for _ in range(n):
     data = list(map(int, input().split()))
