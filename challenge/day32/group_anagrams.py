bfc1cb1a7d9042e9e499b9d0d39c05985f1494f8
freemjstudio <mjwoo0@naver.com>
2024-08-12 14:48:00 +0900

    group anagram

--- challenge/day32/group_anagrams.py
+++ challenge/day32/group_anagrams.py
@@ -28,12 +28,26 @@ Constraints:
 strs[i] consists of lowercase English letters.
 """
 
+from typing import List
+from collections import defaultdict
 
+# frozen set 을 사용할 수 없음 -> dddddg, dggggg 의 경우 같은 anagram 으로 인식하기 때문
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) <= 1:
             return [strs]
-        answer = []
+        str_dict = defaultdict(list)
 
+        for s in strs:
+            k = "".join(sorted(s))
+            str_dict[k].append(s)
 
+        answer = [v for v in str_dict.values()]
         return answer
+
+## TEST CODE
+s = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+result = s.groupAnagrams(strs)
+print(result)
+
