e547acf184c7dc685d14af16300be6271f3d405d
freemjstudio <mjwoo0@naver.com>
2024-08-14 15:00:26 +0900

    교점에 별 만들기

--- 프로그래머스100제/교점에 별 만들기.py
+++ 프로그래머스100제/교점에 별 만들기.py
@@ -9,6 +9,7 @@ def print_arr(arr):
 def solution(line):
     dots = [] # 교점 
     comb = list(combinations(line, 2))
+
     for items in comb:
         l1, l2 = items # line1, line2 
         a, b, e = l1
@@ -22,24 +23,26 @@ def solution(line):
             # print(x, y)
             # 정수인지 확인 
             if int(x) == x and int(y) == y:
-                dots.append((int(x), int(y)))
+                x, y = int(x), int(y)
+                dots.append((x, y))
+    min_x = min(dot[0] for dot in dots)
+    max_x = max(dot[0] for dot in dots)
+    min_y = min(dot[1] for dot in dots)
+    max_y = max(dot[1] for dot in dots)
 
-    print("DOTS", dots)
     # 교점을 배열에 그리기
     if len(dots) == 1: 
         return ["*"]
-    min_x, min_y, max_x, max_y = int(1e9), int(1e9), -int(1e9), -int(1e9) 
-    for x, y in dots: 
-        min_x, min_y = min(min_x, x), min(min_y, y)
-        max_x, max_y = max(max_x, x), max(max_y, y)
-    print(min_x, min_y, max_x, max_y)
-    answer = [['.'] * (max_x - min_x + 1) for _ in range(max_y - min_y + 1)]
-
-    for x, y in dots:
-        answer[y - min_y][x - min_x] = "*" # 이것도 왜 x,y 가 아니라 y,x 인지 모르겠음 
-        print(y - min_y, x - min_x)
-    answer.reverse() # 이걸 이해못함 
-    return ["".join(row) for row in answer]
+    answer = []
+    for y in range(max_y, min_y-1, -1):
+        row = ""
+        for x in range(min_x, max_x+1):
+            if (x, y) in dots: 
+                row += "*"
+            else: 
+                row += "."
+        answer.append(row)
+    return answer 
 
 line = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]
 """
