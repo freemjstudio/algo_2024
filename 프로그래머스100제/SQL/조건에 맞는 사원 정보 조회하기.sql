80f15edc9cb16191304e8a1e68804fc1aeee2d47
freemjstudio <mjwoo0@naver.com>
2024-08-21 11:12:40 +0900

    조건에 맞는 사원 정보 조회하기

--- 프로그래머스100제/SQL/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스100제/SQL/조건에 맞는 사원 정보 조회하기.sql
@@ -19,3 +19,20 @@ WHERE a.SCORE = (
         GROUP BY EMP_NO
     ) max_score
 )
+
+-- 참고한 풀이 
+WITH G_SCORE 
+AS (
+    SELECT EMPNO, sum(SCORE) as SCORE 
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+
+SELECT a.SCORE, a.EMP_NO, b.EMP_NAME, b.POSITION, b.EMAIL 
+FROM G_SCORE a 
+JOIN HR_EMPLOYEES b 
+ON a.EMP_NO = b.EMP_NO
+WHERE score = (
+    SELECT max(SCORE) 
+    FROM G_SCORE
+)
