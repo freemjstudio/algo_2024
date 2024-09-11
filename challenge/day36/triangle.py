d8d30d9709ea5542036377ca14a8a2d497b77b4c
freemjstudio <mjwoo0@naver.com>
2024-09-11 13:25:52 +0900

    [LeetCode] Triangle

--- challenge/day36/triangle.py
+++ challenge/day36/triangle.py
@@ -38,14 +38,13 @@ from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        answer = 0 
         n = len(triangle) # total number of rows
-        dp = [0] * n
-        dp[0] = triangle[0][0] # 시작 
-        j = 0 
-        for i in range(n):
-            
-        return dp[n] 
+
+        for i in reversed(range(n - 1)):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+                
+        return triangle[0][0] 
 
 triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
 
--- challenge/python-grammar/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-async def fetch_data(): # coroutine 반환 
-    print("Fetching data...")
-    await asyncio.sleep(2) 
-    print("Data Fetched!")
-    return {"data":"test"}
-
-async def main():
-    result = await fetch_data() # 비동기 함수의 실행을 중단하고 해당 작업 완료시까지 기다림. 
-    print(result) 
