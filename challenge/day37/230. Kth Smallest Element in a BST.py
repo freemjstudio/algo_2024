78b0c4464486ea627570db073fc214ec58f018ab
freemjstudio <mjwoo0@naver.com>
2024-10-15 22:20:13 +0900

    Kth smallest element in a bst

--- challenge/day37/230. Kth Smallest Element in a BST.py
+++ challenge/day37/230. Kth Smallest Element in a BST.py
@@ -13,6 +13,21 @@ import heapq
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         queue = []
+        """
+        1. BST를 순회하면서 힙에 모든 노드를 넣는다. 
+        2. k-1 번만큼 heap에서 pop 시켜준다. 
+        """
+        def visit_node(root):
+            if root.val:
+                queue.append(root.val)
+            else: 
+                return 
+            if root.left: 
+                visit_node(root.left)
+            if root.right:
+                visit_node(root.right)
+
+        visit_node(root)
         heapq.heapify(queue)
 
         """
