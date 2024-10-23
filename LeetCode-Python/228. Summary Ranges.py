4adcace749867bf6bfebc688538b21388168c7d0
freemjstudio <mjwoo0@naver.com>
2024-10-23 12:55:17 +0900

    228 summary ranges

---
+++ LeetCode-Python/228. Summary Ranges.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/summary-ranges/
+
+"""
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. 
+That is, each element of nums is covered by exactly one of the ranges, 
+and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"] # 이건 정렬된 상태이다. 각각의 숫자는 정확히 하나의 구간 안에 속해야 한다. 
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+
+
+"""
+from typing import List 
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        answer = []
+
+
+        return answer 
--- LeetCode-Python/230. Kth Smallest Element in a BST.py
+++ LeetCode-Python/230. Kth Smallest Element in a BST.py
@@ -9,7 +9,10 @@ class TreeNode:
 
 from typing import Optional 
 
+#### 시도 1
+
 import heapq 
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         queue = []
@@ -28,13 +31,14 @@ class Solution:
                 visit_node(root.right)
 
         visit_node(root)
+        print(queue)
         heapq.heapify(queue)
 
         """
         힙에 모든 노드를 넣고 k-1 번만큼 heap에서 pop 시켜준다. 
         """
-
+        answer = 0
         # heappop 
-        for _ in range(k-1):
+        for _ in range(k):
             answer = heapq.heappop(queue)
         return answer 
